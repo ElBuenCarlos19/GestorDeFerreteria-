46d318f81b10ad4fe85bf85b0a2885151ddd5fc4
Guzman <david.a.guzman26@gmail.com>
2024-09-06 13:15:54 -0500

    Creacion de la base de datos

--- BD/Script DDL.sql
+++ BD/Script DDL.sql
@@ -1,74 +1,74 @@
-create table Role (
-    RoleID int AUTO_INCREMENT,
-    NameRole varchar(100) not null,
-    Description varchar(200) not null,
-    primary key(RoleID)
-)
+CREATE TABLE Role (
+    RoleID SERIAL,
+    NameRole VARCHAR(100) NOT NULL,
+    Description VARCHAR(200) NOT NULL,
+    PRIMARY KEY (RoleID)
+);
 
-create table Users(
-    UserID int AUTO_INCREMENT,
-    Names varchar(100) not null,
-    LastNames varchar(100) not null,
-    Identification int(10) not null unique,
-    Email varchar(100) not null unique,
-    Username varchar(100) not null unique,
-    Password varchar(100) not null,
-    RoleID int not null,
-    primary key(UserID),
-    foreign key(RoleID) references Role(RoleID)
-)
+CREATE TABLE Users (
+    UserID SERIAL,
+    Names VARCHAR(100) NOT NULL,
+    LastNames VARCHAR(100) NOT NULL,
+    Identification INT NOT NULL UNIQUE,
+    Email VARCHAR(100) NOT NULL UNIQUE,
+    Username VARCHAR(100) NOT NULL UNIQUE,
+    Password VARCHAR(100) NOT NULL,
+    RoleID INT NOT NULL,
+    PRIMARY KEY (UserID),
+    FOREIGN KEY (RoleID) REFERENCES Role(RoleID)
+);
 
-create table Client(
-    ClientID int AUTO_INCREMENT,
-    Names varchar(100) not null,
-    LastNames varchar(100) not null,
-    Identification int(10) not null unique,
-    Address varchar(100) not null,
-    PhoneNumber varchar(10) not null,
-    primary key(ClientID),
-)
+CREATE TABLE Client (
+    ClientID SERIAL,
+    Names VARCHAR(100) NOT NULL,
+    LastNames VARCHAR(100) NOT NULL,
+    Identification INT NOT NULL UNIQUE,
+    Address VARCHAR(100) NOT NULL,
+    PhoneNumber VARCHAR(10) NOT NULL,
+    PRIMARY KEY (ClientID)
+);
 
-create table Invoice(
-    InvoiceID int AUTO_INCREMENT,
-    ClientID int not null,
-    UserID int not null,
-    DateTime timestamp default current_timestamp on update current_timestamp,
-    TotalAmount decimal(10, 2) not null,
-    PaymentMethod varchar(100) not null,
-    primary key(InvoiceID),
-    foreign key(ClientID) references Client(ClientID),
-    foreign key(UserID) references Users(UserID)
-)
+CREATE TABLE Invoice (
+    InvoiceID SERIAL,
+    ClientID INT NOT NULL,
+    UserID INT NOT NULL,
+    DateTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    TotalAmount DECIMAL(10, 2) NOT NULL,
+    PaymentMethod VARCHAR(100) NOT NULL,
+    PRIMARY KEY (InvoiceID),
+    FOREIGN KEY (ClientID) REFERENCES Client(ClientID),
+    FOREIGN KEY (UserID) REFERENCES Users(UserID)
+);
 
-create table Provider(
-    ProviderID int AUTO_INCREMENT,
-    ProviderName varchar(100) not null,
-    ProviderAddress varchar(100) not null,
-    PhoneNumber varchar(50) not null,
-    primary key(ProviderID)
-)
+CREATE TABLE Provider (
+    ProviderID SERIAL,
+    ProviderName VARCHAR(100) NOT NULL,
+    ProviderAddress VARCHAR(100) NOT NULL,
+    PhoneNumber VARCHAR(50) NOT NULL,
+    PRIMARY KEY (ProviderID)
+);
 
-create table Product_Inventory(
-    ProductInventoryID int AUTO_INCREMENT,
-    UserID int not null,
-    ProviderID int not null,
-    ProductCode varchar(200) not null unique,
-    ProductName varchar(150) not null,
-    Description varchar(150) not null,
-    Quantity int not null,
-    primary key(ProductInventoryID),
-    foreign key(UserID) references Users(UserID),
-    foreign key(ProviderID) references Provider(ProviderID)
-)
+CREATE TABLE Product_Inventory (
+    ProductInventoryID SERIAL,
+    UserID INT NOT NULL,
+    ProviderID INT NOT NULL,
+    ProductCode VARCHAR(200) NOT NULL UNIQUE,
+    ProductName VARCHAR(150) NOT NULL,
+    Description VARCHAR(150) NOT NULL,
+    Quantity INT NOT NULL,
+    PRIMARY KEY (ProductInventoryID),
+    FOREIGN KEY (UserID) REFERENCES Users(UserID),
+    FOREIGN KEY (ProviderID) REFERENCES Provider(ProviderID)
+);
 
-create table InvoiceDetail(
-    InvoiceDetailID int AUTO_INCREMENT,
-    InvoiceID int not null,
-    ProductInventoryID int not null,
-    Quantity int not null,
-    UnitPrice decimal(10, 2) not null,
-    TotalPrice decimal(10, 2) not null,
-    primary key(InvoiceDetail),
-    foreign key(InvoiceID) references Invoice(InvoiceID),
-    foreign key(ProductInventoryID) references Product_Inventory(ProductInventoryID)
-)
+CREATE TABLE InvoiceDetail (
+    InvoiceDetailID SERIAL,
+    InvoiceID INT NOT NULL,
+    ProductInventoryID INT NOT NULL,
+    Quantity INT NOT NULL,
+    UnitPrice DECIMAL(10, 2) NOT NULL,
+    TotalPrice DECIMAL(10, 2) NOT NULL,
+    PRIMARY KEY (InvoiceDetailID),
+    FOREIGN KEY (InvoiceID) REFERENCES Invoice(InvoiceID),
+    FOREIGN KEY (ProductInventoryID) REFERENCES Product_Inventory(ProductInventoryID)
+);
